028987f8131e0daab0e7143b67c2908161293712
hyowonsong <woniwoni30@gmail.com>
2024-02-19 21:40:20 +0900

    [Add]  SimpleOrderService의 예외 처리

--- src/main/java/kr/co/ordermanagement/domain/exception/NotEnoughAmountException.java
+++ src/main/java/kr/co/ordermanagement/domain/exception/NotEnoughAmountException.java
@@ -1,2 +1,7 @@
-package kr.co.ordermanagement.domain.exception;public class NotEnoughAmountException {
+package kr.co.ordermanagement.domain.exception;
+
+public class NotEnoughAmountException extends RuntimeException {
+    public NotEnoughAmountException(String message) {
+        super(message);
+    }
 }
