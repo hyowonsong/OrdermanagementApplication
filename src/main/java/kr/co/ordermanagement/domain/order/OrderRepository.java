0c59dab6322221ec3b11b8d18fc63de14391fd47
hyowonsong <woniwoni30@gmail.com>
2024-02-19 21:41:28 +0900

    [Add] OrderRepository에 메서드 추가
    SimpleOrderService에 메서드 추가 오류 발생하여 추가

--- src/main/java/kr/co/ordermanagement/domain/order/OrderRepository.java
+++ src/main/java/kr/co/ordermanagement/domain/order/OrderRepository.java
@@ -1,4 +1,10 @@
 package kr.co.ordermanagement.domain.order;
 
+import javax.swing.plaf.nimbus.State;
+import java.util.List;
+
 public interface OrderRepository {
+    Order add(Order order);
+    Order findById(Long id);
+    List<Order> findByState(State state);
 }
