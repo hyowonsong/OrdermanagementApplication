ca3b64cddc119d86f4d007413d1cb17113e781ba
hyowonsong <woniwoni30@gmail.com>
2024-02-19 21:39:47 +0900

    [ Add] SimpleOrderService에 메서드 추가
    createOrder, makeOrderedProducts, decreaseProductsAmount 메서드 추가

--- src/main/java/kr/co/ordermanagement/application/SimpleOrderService.java
+++ src/main/java/kr/co/ordermanagement/application/SimpleOrderService.java
@@ -1,9 +1,16 @@
 package kr.co.ordermanagement.application;
 
+import kr.co.ordermanagement.domain.order.Order;
 import kr.co.ordermanagement.domain.order.OrderRepository;
+import kr.co.ordermanagement.domain.order.OrderedProduct;
+import kr.co.ordermanagement.domain.product.Product;
 import kr.co.ordermanagement.domain.product.ProductRepository;
+import kr.co.ordermanagement.presentation.dto.OrderProductRequestDto;
+import kr.co.ordermanagement.presentation.dto.OrderResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SimpleOrderService {
@@ -15,6 +22,48 @@ public class SimpleOrderService {
     public SimpleOrderService(ProductRepository productRepository, OrderRepository orderRepository) {
         this.productRepository = productRepository;
         this.orderRepository = orderRepository;
+    }
+
+    public OrderResponseDto createOrder(List<OrderProductRequestDto> orderProductRequestDtos) {
+        List<OrderedProduct> orderedProducts = makeOrderedProducts(orderProductRequestDtos);
+        decreaseProductsAmount(orderedProducts);
+
+        Order order = new Order(orderedProducts);
+        orderRepository.add(order);
+
+        OrderResponseDto orderResponseDto = OrderResponseDto.toDto(order);
+        return orderResponseDto;
+    }
+
+    private List<OrderedProduct> makeOrderedProducts(List<OrderProductRequestDto> orderProductRequestDtos) {
+        return orderProductRequestDtos
+                .stream()
+                .map(orderProductRequestDto -> {
+                    Long productId = orderProductRequestDto.getId();
+                    Product product = productRepository.findById(productId);
+
+                    Integer orderedAmount = orderProductRequestDto.getAmount();
+                    product.checkEnoughAmount(orderedAmount);
+
+                    return new OrderedProduct(
+                            productId,
+                            product.getName(),
+                            product.getPrice(),
+                            orderProductRequestDto.getAmount()
+                    );
+                }).toList();
+    }
+
+    private void decreaseProductsAmount(List<OrderedProduct> orderedProducts) {
+        orderedProducts
+                .stream()
+                .forEach(orderedProduct -> {
+                    Long productId = orderedProduct.getId();
+                    Product product = productRepository.findById(productId);
+
+                    Integer orderedAmount = orderedProduct.getAmount();
+                    product.decreaseAmount(orderedAmount);
+                });
     }
 
 }
