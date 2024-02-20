614606f4ca355f1f6197909123e26e6b2bd1dcc3
hyowonsong <woniwoni30@gmail.com>
2024-02-20 16:45:02 +0900

    [Add] Order클래스에 메서드 추가

--- src/main/java/kr/co/ordermanagement/domain/order/Order.java
+++ src/main/java/kr/co/ordermanagement/domain/order/Order.java
@@ -1,4 +1,62 @@
 package kr.co.ordermanagement.domain.order;
 
+import kr.co.ordermanagement.domain.exception.CanNotCancellableStateException;
+
+import java.util.List;
+
 public class Order {
+    private Long id;
+    private List<OrderedProduct> orderedProducts;
+    private Integer totalPrice;
+    private State state;
+
+    public Order(List<OrderedProduct> orderedProducts) {
+        this.orderedProducts = orderedProducts;
+        this.totalPrice = calculateTotalPrice(orderedProducts);
+        this.state = State.CREATED;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<OrderedProduct> getOrderedProducts() {
+        return orderedProducts;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private Integer calculateTotalPrice(List<OrderedProduct> orderedProducts) {
+        return orderedProducts
+                .stream()
+                .mapToInt(orderedProduct -> orderedProduct.getPrice() * orderedProduct.getAmount())
+                .sum();
+    }
+
+    public boolean sameId(Long id) {
+        return this.id.equals(id);
+    }
+
+    public void changeStateForce(State state) {
+        this.state = state;
+    }
+
+    public boolean sameState(State state) {
+        return this.state.equals(state);
+    }
+
+    public void cancel() throws CanNotCancellableStateException {
+        this.state.checkCancellable();
+        this.state = State.CANCELED;
+    }
 }
